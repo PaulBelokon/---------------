a52d049a4e64269b7b0079810c3b73f113e6d186
belokon <belokon.pavlik99@mail.ru>
2023-01-26 19:11:55 +0300

    Write program

--- itogovproect/Program.cs
+++ itogovproect/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "Tome", "Broad", "xD", "15" };
+
+int id = GetArray(array);
+
+GetScore(array, id);
+
+int GetArray(string[] array)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 3)
+        {
+            index++;
+        }
+    }
+    return index;
+}
+
+void GetScore(string[] array, int id)
+{
+    string[] result = new string[id];
+    int n = 0;
+
+    for (int k = 0; k < array.Length; k++)
+    {
+        if (array[k].Length < 3)
+        {
+            result[n] = array[k];
+            n++;
+        }
+    }
+    Console.WriteLine(string.Join(",", result));
+}
